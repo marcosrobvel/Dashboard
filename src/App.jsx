82733fe289bcf77b3faba5ada692b439b5738a0a
marcosrobvel <marcosraranjuez@gmail.com>
2025-02-17 16:50:02 +0100

    goIntoPages1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import Header from './components/Header'
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import MyPhotos from './pages/MyPhotos';
 
-export function App() {
-    return (
-      <div className="App">
-        <Header />
-      </div>
-    );
-  }
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/my-photos" element={<MyPhotos />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import icon_camera from '../assets/img/icon_camera.png';
 import icon_profile from '../assets/img/icon_profile.png';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function Header(){
     return(
         <article>
             <header>
-                <div class='divIcons_And_SearchBar'>
-                    <button class='btnIconCamera'><img src={icon_camera} alt="" /></button>
-                    <div class="divSearchBar">
-                        <button class="btnLens">BtnLupaBusqueda</button><input type="text" placeholder='Buscar . . .' />
+                <div className='divIcons_And_SearchBar'>
+                    <button className='btnIconCamera'><img src={icon_camera} alt="" /></button>
+                    <div className="divSearchBar">
+                        <button className="btnLens">🔍</button><input type="text" placeholder='Buscar . . .' />
                     </div>
-                    <button class='btnIconProfile'><img src={icon_profile} alt="" /></button>
+                    <button className='btnIconProfile'><img src={icon_profile} alt="" /></button>
                 </div>
             </header>
         </article>
