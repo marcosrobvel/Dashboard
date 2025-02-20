85483d3c45729d6a018d8d8e248a3e1277c3496c
marcosrobvel <marcosraranjuez@gmail.com>
2025-02-20 16:27:24 +0100

    tryingToMakeFunctional_MyPhotos2

--- src/components/ImagesComponent.jsx
+++ src/components/ImagesComponent.jsx
@@ -36,15 +36,15 @@ export const ImagesComponent = (images) => {
   };
 
 const handleSave = () => {console.log("HANDLE SAVE");
-  const newLikeState = !likes;        
+  const favourites = !likes;        
   setLikes(favourites);        
-  if (newLikeState) {
-    dispatch(toggleFavourite(image));
+  if (favourites) {
+    dispatch(toggleFavourite(images));
     const dataWithDescription = {
       ...props,                
       description: ""            
     }            
-    icon = "./src/assets/like.svg";            
+    icon = "./src/assets/img/heart.svg";            
     localStorage.setItem(dataWithDescription.id,                
       JSON.stringify(dataWithDescription)            
     );            console.log("SAVED");        
@@ -60,13 +60,13 @@ const handleSave = () => {console.log("HANDLE SAVE");
         <div className="image-grid">
             {images.data.map((images, index) => {
                 const isLiked = likes[index];
-                return <div key={index} className="image-item" onClick={() => openPopup(image.urls.small)}>
+                return <div key={index} className="image-item" onClick={() => openPopup(images.urls.small)}>
                     <div className="divAllImages">
                     <img
                                     src={images.urls.small}
                                     alt="image"
                                     className="cardImage"
-                                    onClick={() => openPopup(image.urls.small)}
+                                    onClick={() => openPopup(images.urls.small)}
                                 />
                                 {isLiked ? (
                                     <svg
@@ -77,8 +77,10 @@ const handleSave = () => {console.log("HANDLE SAVE");
                                         className="heartLikesClicked"
                                         viewBox="0 0 16 16"
                                         onClick={(e) => {
-                                          e.stopPropagation();  // Evitar que el clic se propague al contenedor
-                                          handleSave(image);  // Llamamos a handleSave para quitarla de favoritos
+                                          e.stopPropagation();
+                                          handleSave(images);
+                                          toggleLike(index);  // Evitar que el clic se propague al contenedor
+                                        //  handleSave(images);  // Llamamos a handleSave para quitarla de favoritos
                                       }}
                                     >
                                         <path
@@ -95,8 +97,10 @@ const handleSave = () => {console.log("HANDLE SAVE");
                                         className="heartLikes"
                                         viewBox="0 0 16 16"
                                         onClick={(e) => {
-                                          e.stopPropagation();  // Evitar que el clic se propague al contenedor
-                                          handleSave(image);  // Llamamos a handleSave para agregarla a favoritos
+                                          e.stopPropagation(); 
+                                          handleSave(images);
+                                          toggleLike(index); // Evitar que el clic se propague al contenedor
+                                          // Llamamos a handleSave para agregarla a favoritos
                                       }}
                                     >
                                         <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
