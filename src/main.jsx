e2142b1dc94a7bea36970efa1f66496fa8046663
marcosrobvel <marcosraranjuez@gmail.com>
2025-03-06 09:37:18 +0100

    deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,25 +2,27 @@ import React, { StrictMode } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import './sass/styles.scss';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { Layout } from './components/Layout';
 import { Home } from './pages/Home';
 import { MyPhotos } from './pages/MyPhotos';
+import { Navigate } from 'react-router-dom';
 
 //import MyPhotos from './pages/MyPhotos';
 
 createRoot(document.getElementById('root')).render( 
    
       <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
               <Route element={<Layout/>}>
+                <Route path="/" element={<Navigate to="/dashboard" />} />
                 <Route path='dashboard' element={< Home />} />
                 <Route path='dashboard/myphotos' element={< MyPhotos />} />
               </Route>
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </Provider>
 );
