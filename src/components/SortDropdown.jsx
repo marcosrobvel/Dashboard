999358a0d37a7298253fcb82c5d86bbfb7b0db3c
marcosrobvel <marcosraranjuez@gmail.com>
2025-03-11 10:12:16 +0100

    fixingOrderOption

--- src/components/SortDropdown.jsx
+++ src/components/SortDropdown.jsx
@@ -1,43 +1,23 @@
 import React, { useState } from 'react';
 
 export const SortDropdown = ({ onSortChange }) => {
-    const [sortCriteria, setSortCriteria] = useState('date');
-    const [sortDirection, setSortDirection] = useState('asc');
-
-    const handleCriteriaChange = (e) => {
-        const newCriteria = e.target.value;
-        setSortCriteria(newCriteria);
-        onSortChange(newCriteria, sortDirection);
-    };
-
-    const handleDirectionChange = (e) => {
-        const newDirection = e.target.value;
-        setSortDirection(newDirection);
-        onSortChange(sortCriteria, newDirection); 
-    };
-
-    return (
-        <div className="sort-dropdown">
-            <select
-                value={sortCriteria}
-                onChange={handleCriteriaChange}
-                className="dropdownFilterOrder"
-            >
-                <option className='orderByOption' value="">Order by</option>
-                <option value="date">Import date</option>
-                <option value="width">Width</option>
-                <option value="height">Height</option>
-                <option value="likes">Likes</option>
-            </select>
-
-            <select
-                value={sortDirection}
-                onChange={handleDirectionChange}
-                className="dropdownFilterOrderAscDesc"
-            >
-                <option value="asc">Asc</option>
-                <option value="desc">Desc</option>
-            </select>
-        </div>
-    );
+ 
+    const handleChange = (e) => {
+        const [criteria, direction] = e.target.value.split('_');
+        onSortChange(criteria, direction);
+      };
+    
+      return (
+        <select className="dropdownOrder" onChange={handleChange}>
+            <option value="" disabled selected>Order by</option>
+          <option value="date_asc">Import date ↑</option>
+          <option value="date_desc">Import date ↓</option>
+          <option value="likes_asc">Likes ↑</option>
+          <option value="likes_desc">Likes ↓</option>
+          <option value="width_asc">Width ↑</option>
+          <option value="width_desc">Width ↓</option>
+          <option value="height_asc">Height ↑</option>
+          <option value="height_desc">Height ↓</option>
+        </select>
+      );
 };
